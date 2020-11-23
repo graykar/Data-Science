b6437e74f2a73bdfd08ab50d94038163e84ec5ec
graykar <58473294+graykar@users.noreply.github.com>
2020-11-23 18:59:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,9 +2,11 @@
 from flask import Flask, render_template, request
 import pickle
 import nltk
+from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 LMT = WordNetLemmatizer()
 nltk.download('wordnet')
+nltk.download('stopwords')
 
 # Load the Multinomial Naive Bayes model and CountVectorizer object from disk
 filename = 'Reasturent_Sentiment_Multinominal_Model.pkl'
@@ -21,9 +23,9 @@ def home():
 def predict():
     if request.method == 'POST':
     	message = request.form['message']
-    	data = [message]
-		mess = [LMT.lemmatize(x) for x in data.lower().split() if not x in set(stopwords.words('english')) - {'not'}]
-        mess = " ".join(mess)	
+		mess = [LMT.lemmatize(x) for x in message.lower().split() if not x in set(stopwords.words('english')) - {'not'}]
+		mess = " ".join(mess)
+    	data = [mess]			
     	vect = cv.transform(mess).toarray()
     	my_prediction = classifier.predict(vect)
     	return render_template('result.html', prediction=my_prediction)
